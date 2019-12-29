cd0b05e4fc6445ead8b1ae2da42f31b2696a4a6a
fkaiser <fabian.kaiser@wingtra.com>
2019-12-29 18:29:21 +0100

    make it a class

--- motion_tracker.py
+++ motion_tracker.py
@@ -3,40 +3,56 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def get_ROI(img):
-    # Select ROI
-    fromCenter = False
-    r = cv2.selectROI(img, fromCenter)
+class motionTracker:
 
-    # Crop image
-    height = int(r[1]+ r[3]) - int(r[1])
-    width = int(r[0] + r[2]) - int(r[0])
-    im_crop = img[int(r[1]):int(r[1]+ r[3]), int(r[0]):int(r[0] + r[2])]
-    target_ROI = {'image': im_crop, 'height': height, width: 'width'}
-    return target_ROI
+    def __init__(self, image_path, n_particles=10):
+        self.n_particles = n_particles
+        self.image_path = image_path
+        self.image = self.load_image(self.image_path)
+        self.target_ROI = self.get_ROI(self.image)
+        self.get_target_hist()
 
+    def get_ROI(self, img):
+        # Select ROI
+        fromCenter = False
+        r = cv2.selectROI(img, fromCenter)
 
-def convert_to_grayscale(img):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    return gray
+        # Crop image
+        height = int(r[1] + r[3]) - int(r[1])
+        width = int(r[0] + r[2]) - int(r[0])
+        im_crop = img[int(r[1]):int(r[1] + r[3]), int(r[0]):int(r[0] + r[2])]
+        target_ROI = {'image': im_crop, 'image_gray':
+                      self.convert_to_grayscale(im_crop),
+                      'height': height, width: 'width'}
+        return target_ROI
 
+    def load_image(self, image_path):
+        im = cv2.imread(image_path)
+        return im
 
-def get_target_hist(image_path):
-    im = cv2.imread('image0203.png')
-    im_gray = convert_to_grayscale(im)
-    target_ROI = get_ROI(im_gray)
-    plt.subplot(121), plt.imshow(target_ROI['image'], 'gray')
-    plt.subplot(122)
-    target_hist = dict()
-    target_hist['values'], target_hist['bins'], _ = plt.hist(target_ROI['image'].ravel(),50,[0,256])
-    plt.xlim(0,256)
-    plt.show()
-    return hist_gray
+    def get_target_hist(self):
+        im_gray = self.convert_to_grayscale(self.image)
+        plt.subplot(121), plt.imshow(self.target_ROI['image_gray'], 'gray')
+        plt.subplot(122)
+        self.target_hist = dict()
+        self.target_hist['values'], self.target_hist['bins'], _ = plt.hist(
+            self.target_ROI['image_gray'].ravel(), 50, [0, 256])
+        plt.xlim(0, 256)
+        plt.show()
+
+    @staticmethod
+    def convert_to_grayscale(img):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        return gray
+
+    def init_particles(self):
+        pass
+
 
 
 def main():
     image_path = 'image0203.png'
-    target_hist = get_target_hist(image_path)
+    ping_pong_tracker = motionTracker(image_path=image_path)
 
 
 if __name__ == '__main__':
