c5ddddabd602055837f1896d3b6e9e5606223ffa
fkaiser <fabian.kaiser@wingtra.com>
2020-05-10 18:56:45 +0200

    use image transport

--- online_tracker/image_handler.cpp
+++ online_tracker/image_handler.cpp
@@ -2,6 +2,7 @@
 #include "sensor_msgs/CompressedImage.h"
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
 #include <opencv2/opencv.hpp>
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
@@ -29,10 +30,8 @@ void image_fetch_callback(const sensor_msgs::CompressedImage& msg)
   cv::medianBlur(gray, gray, 5);
   std::vector<cv::Vec3f> circles;
   cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
-                 gray.rows/16,  // change this value to detect circles with different distances to each other
-                 100, 30, 20, 100 // change the last two parameters
-            // (min_radius & max_radius) to detect larger circles
-    );
+                 gray.rows/16,
+                 100, 30, 20, 100);
     ROS_INFO("Found circles: %d",int(circles.size()));
     for( size_t i = 0; i < circles.size(); i++ )
     {
@@ -59,13 +58,86 @@ void image_fetch_callback(const sensor_msgs::CompressedImage& msg)
 
 }
 
-int main(int argc, char **argv)
+// int main(int argc, char **argv)
+// {
+//   ros::init(argc, argv, "listener");
+//   ros::NodeHandle n;
+//   ros::Subscriber sub = n.subscribe("/raspicam_node/image/compressed", 1000, image_fetch_callback);
+//   image_transport::Publisher image_pub = it_.advertise("/image_converter/output_video", 1);
+
+//   ros::spin();
+
+//   return 0;
+// }
+
+static const std::string OPENCV_WINDOW = "Image window";
+
+class ImageConverter
 {
-  ros::init(argc, argv, "listener");
-  ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("/raspicam_node/image/compressed", 1000, image_fetch_callback);
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
 
+public:
+  ImageConverter()
+    : it_(nh_)
+  {
+    // Subscribe to input video feed and publish output video feed
+    image_sub_ = it_.subscribe("/raspicam_node/image/compressed", 1, &ImageConverter::imageCb, this);
+    image_pub_ = it_.advertise("/image_converter/output_video", 1);
+
+    //cv::namedWindow(OPENCV_WINDOW);
+  }
+
+  ~ImageConverter()
+  {
+    //cv::destroyWindow(OPENCV_WINDOW);
+  }
+
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Find ball and draw it
+    cv::Mat src = cv_ptr->image;
+    cv::Mat gray;
+    cv::cvtColor(src, gray, cv::COLOR_BGR2GRAY);
+    cv::medianBlur(gray, gray, 5);
+    std::vector<cv::Vec3f> circles;
+    cv::HoughCircles(gray, circles, cv::HOUGH_GRADIENT, 1,
+                  gray.rows/16,
+                  100, 30, 20, 100);
+      ROS_INFO("Found circles: %d",int(circles.size()));
+      for( size_t i = 0; i < circles.size(); i++ )
+      {
+          cv::Vec3i c = circles[i];
+          cv::Point center = cv::Point(c[0], c[1]);
+          // circle center
+          circle(src, center, 1, cv::Scalar(0,100,100), 3, cv::LINE_AA);
+          // circle outline
+          int radius = c[2];
+          circle(src, center, radius, cv::Scalar(255,0,255), 3, cv::LINE_AA);
+      }
+
+      // Output modified video stream
+      image_pub_.publish(cv_ptr->toImageMsg());
+    }
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_converter");
+  ImageConverter ic;
   ros::spin();
-
   return 0;
 }
