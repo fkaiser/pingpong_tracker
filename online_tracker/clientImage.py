f6be87db2b4ccf3e5b7be0cc552a2a946f69d66a
fkaiser <fabian.kaiser@wingtra.com>
2020-06-10 17:46:44 +0200

    do not stream them

--- online_tracker/clientImage.py
+++ online_tracker/clientImage.py
@@ -28,17 +28,17 @@ try:
     for foo in camera.capture_continuous(stream, 'jpeg'):
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
-        connection.write(struct.pack('<L', stream.tell()))
-        connection.flush()
+        #connection.write(struct.pack('<L', stream.tell()))
+        #connection.flush()
         # Rewind the stream and send the image data over the wire
-        stream.seek(0)
-        connection.write(stream.read())
+        #stream.seek(0)
+        #connection.write(stream.read())
         # If we've been capturing for more than 30 seconds, quit
         if time.time() - start > 30:
             break
         # Reset the stream for the next capture
-        stream.seek(0)
-        stream.truncate()
+        #stream.seek(0)
+        #stream.truncate()
         counter += 1
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
