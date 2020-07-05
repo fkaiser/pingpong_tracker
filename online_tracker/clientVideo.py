603519b75d687f6587f9da2f25a4c6014d218c9d
fkaiser <fabian.kaiser@wingtra.com>
2020-07-05 14:21:35 +0200

    make ip and port input

--- online_tracker/clientVideo.py
+++ online_tracker/clientVideo.py
@@ -1,26 +1,50 @@
 import socket
 import time
 import picamera
+import os
+import sys
+import argparse
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
-client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-client_socket.connect(('192.168.1.13', 5001))
+def stream_via_udp(ip, port):
+    client_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    client_socket.connect((ip, port))
 
-# Make a file-like object out of the connection
-connection = client_socket.makefile('wb')
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640, 480)
-    camera.framerate = 80
-    # Start a preview and let the camera warm up for 2 seconds
-    camera.start_preview()
-    time.sleep(2)
-    # Start recording, sending the output to the connection for 60
-    # seconds, then stop
-    camera.start_recording(connection, format='h264')
-    camera.wait_recording(36000)
-    camera.stop_recording()
-finally:
-    connection.close()
-    client_socket.close()
+    # Make a file-like object out of the connection
+    connection = client_socket.makefile('wb')
+    try:
+        camera = picamera.PiCamera()
+        camera.resolution = (640, 480)
+        camera.framerate = 80
+        # Start a preview and let the camera warm up for 2 seconds
+        camera.start_preview()
+        time.sleep(2)
+        # Start recording, sending the output to the connection for 60
+        # seconds, then stop
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(36000)
+        camera.stop_recording()
+    finally:
+        connection.close()
+        client_socket.close()
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('ip',
+                        help='IP of receiver')
+    parser.add_argument('--port', default=5000, type=int,
+                        help='Port to send stream to.')
+    args = parser.parse_args()
+    stream_via_udp(ip=args.ip, port=args.port)
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
