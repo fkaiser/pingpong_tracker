93f63daa2da18cb9ca05a48e9d91c5e9379213a4
fkaiser <fabian.kaiser@wingtra.com>
2020-07-05 13:14:19 +0100

    client improvements

--- online_tracker/clientVideo.py
+++ online_tracker/clientVideo.py
@@ -12,14 +12,14 @@ connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
     camera.resolution = (640, 480)
-    camera.framerate = 24
+    camera.framerate = 80
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
     time.sleep(2)
     # Start recording, sending the output to the connection for 60
     # seconds, then stop
     camera.start_recording(connection, format='h264')
-    camera.wait_recording(60)
+    camera.wait_recording(36000)
     camera.stop_recording()
 finally:
     connection.close()
