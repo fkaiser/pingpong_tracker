3d1ec1bc61e1efe6979e6aad7953a6a23247b89f
fkaiser <fabian.kaiser@wingtra.com>
2020-07-26 20:12:10 +0200

    Add proper arithmetic and allow odd number of pixels in height and width

--- video_from_frames.sh
+++ video_from_frames.sh
@@ -7,7 +7,7 @@ for d in $1/*; do
         match_number=${BASH_REMATCH[2]}
         if [[ ${start_frame} == "" ]]; then
             start_frame=${match_number}
-            digits=${#BASH_REMATCH[1]} + ${#BASH_REMATCH[2]}
+            let digits=${#BASH_REMATCH[1]}+${#BASH_REMATCH[2]}
             echo ${BASH_REMATCH[0]}
         fi
         if [[ ${start_frame} -gt  ${match_number} ]]; then    
@@ -18,10 +18,9 @@ for d in $1/*; do
 done
 
 digits=$((${digits}))
-digits=4
 frame_match=$1"/image%0"${digits}"d.png"
 echo  "Start frame is: " ${start_frame}
 echo "digits: " ${digits}
 echo "name of video file: " ${output_name}
 
-ffmpeg -start_number ${start_frame} -framerate ${frame_rate} -i ${frame_match} -pix_fmt yuv420p ${output_name}
+ffmpeg -start_number ${start_frame} -framerate ${frame_rate} -i ${frame_match} -vcodec libx264 -y -an ${output_name} -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2"
